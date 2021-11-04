bda1dd7e0a41de5a127b87043c1d729bdc5fe66e
Luca Di Molfetta <63596667+dimolf345@users.noreply.github.com>
2021-11-04 17:09:23 +0100

    add item and remove item functionality implemented

--- src/redux/cart/cart.utils.js
+++ src/redux/cart/cart.utils.js
@@ -17,3 +17,25 @@ export const addItemToCart = (cartItems, cartItemToAdd) => {
   //quantity property
   return [...cartItems, { ...cartItemToAdd, quantity: 1 }];
 };
+
+export const remoteItemFromCart = (cartItems, cartItemToRemove) => {
+  //checks if the itemToremove is in the cartItems array
+  const existingCartItem = cartItems.find(
+    (cartItem) => cartItem.id === cartItemToRemove.id
+  );
+
+  //if there is only one, we remove it from the cartItems with
+  //filter method
+  if (existingCartItem.quantity === 1) {
+    //if the id is not the one we want to remove, we keep it
+    return cartItems.filter((cartItem) => cartItem.id !== cartItemToRemove.id);
+  }
+
+  //if quantity is greater than 1, we map the array and reduce the
+  //quantity of cartItem that has the same id of cartItemToRemove
+  return cartItems.map((cartItem) =>
+    cartItem.id === cartItemToRemove.id
+      ? { ...cartItem, quantity: cartItem.quantity - 1 }
+      : cartItem
+  );
+};
